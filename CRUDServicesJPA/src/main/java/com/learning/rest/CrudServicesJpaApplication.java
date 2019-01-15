80a2d98a522dd86c82e4cae2a25766cd57a89232
Viswa <Viswa@DESKTOP-ECAV453>
2019-01-15 22:19:25 +0530

    Adding New Project

---
+++ CRUDServicesJPA/src/main/java/com/learning/rest/CrudServicesJpaApplication.java
@@ -0,0 +1,14 @@
+package com.learning.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudServicesJpaApplication.class, args);
+	}
+
+}
+
