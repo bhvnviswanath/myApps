39a2c1d9462a0147cded6fc7cd267054a73cfba8
Viswa <Viswa@DESKTOP-ECAV453>
2019-06-06 23:34:20 +0530

    String to Java Object conversion

---
+++ jsonToObject/src/main/java/com/convert/json/App.java
@@ -0,0 +1,35 @@
+package com.convert.json;
+
+import java.io.File;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String json = "{\"id\": 1, \"name\": \"The Beatles\"}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Object artist = mapper.readValue(json, Object.class);
+            System.out.println("Artist = " + artist);
+            Map<String,Object> map = mapper.readValue(json, Map.class);
+            System.out.println(map.values().size());
+            
+            for (String key : map.keySet()) {
+            	System.out.println(key);
+    			System.out.println(map.get(key));
+    		}
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	
+    }
+}
