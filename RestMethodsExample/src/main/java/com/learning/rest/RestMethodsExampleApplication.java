92e935ac996ba3250b4161d2dd66054de715df97
Viswa <Viswa@DESKTOP-ECAV453>
2019-06-30 12:17:27 +0530

    Adding new Project - Rest Method Example

---
+++ RestMethodsExample/src/main/java/com/learning/rest/RestMethodsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.learning.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMethodsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMethodsExampleApplication.class, args);
+	}
+
+}
