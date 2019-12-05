80c6f0ad03b30d2452c5fd0eaa924d92e1f19c8f
Viswa <Viswa@DESKTOP-ECAV453>
2019-12-06 00:18:39 +0530

    updated

--- CRUDServicesJPA/src/main/java/com/learning/rest/model/ErrorResponse.java
+++ CRUDServicesJPA/src/main/java/com/learning/rest/model/ErrorResponse.java
@@ -1,5 +1,11 @@
 package com.learning.rest.model;
 
+import java.util.List;
+
 public class ErrorResponse {
+
+	public ErrorResponse(String string, List<String> details) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
