620833069404ece3a8007c5df97462bd9cea5514
Viswa <Viswa@DESKTOP-ECAV453>
2020-01-01 22:24:12 +0530

    Added Project Spring Zuul

---
+++ spring-cloud-zuul-api-gateway/src/main/java/com/learning/gateway/SpringCloudZuulApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.learning.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class SpringCloudZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulApiGatewayApplication.class, args);
+	}
+
+}
